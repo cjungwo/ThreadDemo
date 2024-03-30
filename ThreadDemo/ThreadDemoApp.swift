c7b94351cced88dff561d08629ac2ef24de6e81e
Cjungwo <jwoo516@naver.com>
2024-03-30 16:42:56 +1100

    Initial Commit

---
+++ ThreadDemo/ThreadDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadDemoApp.swift
+//  ThreadDemo
+//
+//  Created by JungWoo Choi on 30/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct ThreadDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
